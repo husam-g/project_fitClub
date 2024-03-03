e904aa2339297b714ecdc73b8c72406b022beb13
Hüsam <husamahmud@gmail.com>
2024-03-04 01:05:47 +0300

    Edit Footer Component import

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Hero from "./components/hero/Hero.jsx"
-import Programs from "./components/programs/Programs.jsx"
-import Reasons from "./components/reasons/Reasons.jsx"
-import Plans from "./components/plans/Plans.jsx"
-import Testimonials from "./components/testimonials/Testimonials.jsx"
-import JoinUs from "./components/joinus/JoinUs.jsx"
-import Footer from "./components/footer/footer.jsx"
+import Hero from './components/hero/Hero.jsx';
+import Programs from './components/programs/Programs.jsx';
+import Reasons from './components/reasons/Reasons.jsx';
+import Plans from './components/plans/Plans.jsx';
+import Testimonials from './components/testimonials/Testimonials.jsx';
+import JoinUs from './components/joinus/JoinUs.jsx';
+import Footer from './components/footer/Footer.jsx';
 
 const App = () => (
   <div className="App">
@@ -16,6 +16,6 @@ const App = () => (
     <JoinUs />
     <Footer />
   </div>
-)
+);
 
-export default App
+export default App;
